6247938fe43d58697c2d88909d978ffcb453a6e2
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-04-25 16:21:14 -0300

    feature: jump key holding to increase jump height

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,9 +1,11 @@
 extends Actor
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	var is_jump_released:= Input.is_action_just_released("jump") and velocity.y < 0
+	
 	var direction := get_input_direction()
 	
-	velocity = calculate_move_velocity(velocity, direction, speed)
+	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_released)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 
 
@@ -14,7 +16,8 @@ func get_input_direction() -> Vector2:
 	-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0)
 
 
-func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2) -> Vector2:
+func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2, 
+															is_jump_released: bool) -> Vector2:
 	var move_velocity:= linear_velocity
 	
 	move_velocity.x = direction.x * speed.x
@@ -22,5 +25,8 @@ func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed
 	
 	if direction.y == -1.0:
 		move_velocity.y = speed.y * direction.y
+		
+	if is_jump_released:
+		move_velocity.y = 0.0
 	
 	return move_velocity
