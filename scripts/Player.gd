c40ba50fbd44ca8b35d2ee244b44f2cc82eb1547
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-04-28 17:43:39 -0300

    feature: player attack and animation state machine

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,10 @@
 extends Actor
 
+var anim_state_machine: AnimationNodeStateMachinePlayback
+
+func _ready():
+	anim_state_machine = $AnimationTree.get("parameters/playback")
+
 func _physics_process(_delta):
 	var is_jump_released:= Input.is_action_just_released("jump") and velocity.y < 0
 	
@@ -7,6 +12,9 @@ func _physics_process(_delta):
 	
 	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_released)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	
+	handle_animation(velocity)
+	handle_attack_input()
 
 
 func get_input_direction() -> Vector2:
@@ -15,9 +23,15 @@ func get_input_direction() -> Vector2:
 	return Vector2(horizontal,
 	-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0)
 
+func handle_attack_input():
+	if Input.is_action_just_pressed("attack"):
+		anim_state_machine.travel("attack")
 
 func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2, 
 															is_jump_released: bool) -> Vector2:
+	if(anim_state_machine.get_current_node() == "attack"):
+		return Vector2.ZERO
+	
 	var move_velocity:= linear_velocity
 	
 	move_velocity.x = direction.x * speed.x
@@ -30,3 +44,16 @@ func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed
 		move_velocity.y = 0.0
 	
 	return move_velocity
+
+func handle_animation(current_velocity: Vector2) -> void:
+	if current_velocity == Vector2.ZERO:
+		anim_state_machine.travel("idle")
+		return
+	
+	if current_velocity.y < 0:
+		anim_state_machine.travel("jump")
+		return
+	
+	if current_velocity.x != 0:
+		anim_state_machine.travel("walk")
+	
