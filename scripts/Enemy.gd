1c4c836e44f03cb39a48aa021bb5b79e4029de34
Bruno Sahlberg <bruno040101@gmail.com>
2022-05-01 21:15:31 -0300

    feature: add enemy

---
+++ scripts/Enemy.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+var speed = Vector2(300.0,1000.0)
+var side = -1
+var gravity= 4000.0
+var velocity = Vector2.ZERO
+
+func _ready():
+	pass
+	set_physics_process(false)
+	velocity.x = -speed.x
+	
+	#$RaydCast2D.position.x = $CollisionShape2D.shape.get_extents().x * side
+
+func _physics_process(delta):
+	$AnimatedSprite.scale.x = side * 1
+	velocity.y += gravity * delta
+	if is_on_wall():
+		velocity.x *= -1.0
+		side = 1
+		#$RayCast2D.position.x = $CollisionShape2D.shape.get_extents().x * side
+	
+	velocity.y = move_and_slide(velocity, Vector2.UP).y
+
+
+#not $RayCast2D.is_colliding() and is_on_floor()
