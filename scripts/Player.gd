411617f41b30d332cb56e5e27e27f1df480299e1
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-05-23 17:13:26 -0300

    feature: player dying state and animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -10,6 +10,7 @@ func _ready():
 
 func _physics_process(_delta):
 	current_state = anim_state_machine.get_current_node()
+	print(current_state)
 	
 	var is_jump_released:= Input.is_action_just_released("jump") and _velocity.y < 0
 	
@@ -32,7 +33,7 @@ func get_input_direction() -> Vector2:
 func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2, 
 															is_jump_released: bool) -> Vector2:
 	
-	if(current_state == "attack" and is_on_floor()):
+	if (current_state == "attack" and is_on_floor()) or current_state == "die" :
 		return Vector2.ZERO
 	
 	var move_velocity:= linear_velocity
@@ -49,6 +50,9 @@ func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed
 	return move_velocity
 
 func handle_animation(current_velocity: Vector2) -> void:
+	if current_state == "take_damage" or current_state == "die":
+		return
+	
 	if current_velocity == Vector2.ZERO:
 		anim_state_machine.travel("idle")
 		return
@@ -68,6 +72,9 @@ func handle_animation(current_velocity: Vector2) -> void:
 	
 	
 func handle_attack_input():
+	if current_state == "die":
+		return
+	
 	if Input.is_action_just_pressed("attack") == false:
 		return
 	
@@ -86,4 +93,18 @@ func _on_AttackHitbox_area_entered(area):
 
 
 func _on_Hurtbox_area_entered(area):
-	pass # Replace with function body.
+	take_damage()
+
+func take_damage():
+	hp -= 1
+	print(hp)
+	if hp <= 0:
+		die()
+		return
+
+	anim_state_machine.travel("take_damage")
+
+func die():
+	anim_state_machine.travel("die")
+	$Hurtbox/CollisionShape2D.disabled = true
+	#$AnimationTree.active = false
