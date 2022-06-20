7af9489d4f1836d471c242e781bb5ae48958a453
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-06-20 19:28:35 -0300

    feature: pick up key

---
+++ scripts/Key.gd
@@ -0,0 +1,12 @@
+extends "res://scripts/Item.gd"
+
+export var heal_amount := 1
+
+func _on_Key_area_entered(area):
+	var player_root = area.get_node("..")
+	if player_root is Player:
+		apply_effect(player_root)
+		queue_free()
+
+func apply_effect(player: Player):
+	player.grab_key()
