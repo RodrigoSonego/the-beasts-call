1c147c76319964511e626cb8114821d0e9196d6e
Bruno Sahlberg <bruno040101@gmail.com>
2022-06-25 17:17:41 -0300

    wip: menu scene

---
+++ scripts/menu/BotaoJogar.gd
@@ -0,0 +1,23 @@
+extends Button
+
+func _on_Jogar_mouse_entered():
+	$Sprite.modulate.r = 0.65
+	$Sprite.modulate.g = 0.65
+	$Sprite.modulate.b = 0.65
+	
+	pass # Replace with function body.
+
+
+func _on_Jogar_mouse_exited():
+	$Sprite.modulate.r = 1
+	$Sprite.modulate.g = 1
+	$Sprite.modulate.b = 1
+	pass # Replace with function body.
+
+
+func _on_Jogar_pressed():
+	$Sprite.modulate.r = 0.5
+	$Sprite.modulate.g = 0.5
+	$Sprite.modulate.b = 0.5
+	get_tree().change_scene("res://scenes/levels/GameManager.tscn")
+	pass # Replace with function body.
