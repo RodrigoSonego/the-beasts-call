d8c2805dc4d0edf8f998428b7069e02bf6056834
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-06-27 00:06:46 -0300

    feature: locked door indication

--- scripts/items/Door.gd
+++ scripts/items/Door.gd
@@ -3,9 +3,13 @@ class_name Door
 
 signal on_door_interaction()
 export var is_locked:= false
+onready var locked_sprite := $LockedSprite
 
 var is_player_in_range:= false
 var player_has_key := false
+
+func _ready():
+	locked_sprite.hide()
 
 func _physics_process(delta):
 	if(is_player_in_range == false): return
@@ -19,6 +23,7 @@ func _physics_process(delta):
 
 func _on_Door_area_entered(area):
 	is_player_in_range = true
+	if is_locked: locked_sprite.show()
 	
 	var player: Player
 	player = area.get_node("..")
@@ -27,3 +32,4 @@ func _on_Door_area_entered(area):
 
 func _on_Door_area_exited(area):
 	is_player_in_range = false
+	locked_sprite.hide()
